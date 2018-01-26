22bb857e84c3083d8d1c5f450811ef5b925201c4
sec <memeyun2000@126.com>
2018-01-26 12:42:31 +0800

    .

---
+++ src/main/resources/static/js/model/modelList.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // $("button[name='updateRecord']").bind("click",function(){
+    //     var _model = $("input[name='checkboxModel']:checkbox:checked").val();
+    //     console.log(_model.getMemo());
+    //     /*<![CDATA[*/  
+    //     // var openid = /*[[${_model}]]*/;//取值  
+    //     /*]]>*/ 
+    // });
+
+});
